dd47321049e7564efea2e5f8859089d26e65b1d5
TheCreator888 <163675949+TheCreator888@users.noreply.github.com>
2024-03-16 21:15:12 +0100

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,27 @@
+let allMusic = [
+    {
+      name: "Look at Me Now",
+      artist: "Brennan Savage",
+      src: "Brennan Savage-Look at Me Now.mp3"
+    },
+    {
+      name: "Day 'n' Night",
+      artist: "Kid Cudi",
+      src: "Kid Cudi-Day 'n' Nite.mp3"
+    },
+    {
+      name: "Got It On Me",
+      artist: "Pop Smoke",
+      src: "Pop Smoke-Got It On Me.mp3"
+    },
+    {
+      name: "Bulletproof",
+      artist: "Brennan Savage",
+      src: "Brennan Savage-Bulletproof.mp3"
+    },
+    {
+      name: "Benz Truck",
+      artist: "Lil Peep",
+      src: "Lil Peep-Benz Truck (гелик).mp3"
+    }
+  ];
